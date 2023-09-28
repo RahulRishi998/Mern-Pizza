6a8d7549f13110ca04dd1522ff9769e78d08cde2
rahul <rahul@gmail.com>
2023-09-28 11:53:50 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express()
 const Port = process.env.PORT||5000
-require("./db/conn")
-const Pizza = require("./db/model")
-const pizzaRoutes = require("./routes/pizzasRoutes")
-const userRoutes = require("./routes/userRoutes")
-const orderRoutes = require("./routes/orderRoutes")
+require("./src/db/conn")
+const Pizza = require("./src/db/model")
+const pizzaRoutes = require("./src/routes/pizzasRoutes")
+const userRoutes = require("./src/routes/userRoutes")
+const orderRoutes = require("./src/routes/orderRoutes")
 const cors = require("cors")
 
 app.use(express.urlencoded({extended:true}))
